c27a9cb0afb94902588b9b81a06d24053182c88d
donlpublic <71415614+donlpublic@users.noreply.github.com>
2022-10-29 23:49:54 -0700

    Update caixin.js

--- caixin.js
+++ caixin.js
@@ -3,7 +3,4 @@ var url=$request.url.replace(/uid=(\d+|)/g,"uid=12622061")
             .replace(/code=(\w+|)/g,"code=7258E07D155FE91FB868E545947DDCAB")
             .replace(/device=(\w+|)/g,"device=af491839f424cf75f07d7f4d6b7a30bde3296ea2")
             .replace(/deviceType=(\d+|)/g,"deviceType=1");
-if($request['url']['indexOf']('validateAudioAuth')!=-0x1){
-            header['appinfo']='ddgksf2013';
-}
 $done({'url':url,'headers':header});
